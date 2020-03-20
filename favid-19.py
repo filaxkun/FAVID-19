39db229466f883d2add91aba51db54eb9305a0c3
Filax <fls.francesco@gmail.com>
2020-03-20 14:40:36 +0100

    Added CSVs as external variables

--- favid-19.py
+++ favid-19.py
@@ -1,16 +1,19 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import mycsv #add location to your csv
 
-confirmed = '/home/filax/git/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv'
-recovered = '/home/filax/git/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv'
-deaths = '/home/filax/git/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv'
-who = '/home/filax/git/COVID-19/who_covid_19_situation_reports/who_covid_19_sit_rep_time_series/who_covid_19_sit_rep_time_series.csv'
+df_conf = pd.read_csv(mycsv.confirmed)
 
-df_conf = pd.read_csv(confirmed)
+print(df_conf)
+
+# Filter for Italy
+df_conf = df_conf[df_conf['Country/Region'] == 'Italy']
 
 df_conf.set_index( 'Country/Region' , inplace = True)
 df_conf = df_conf.drop(['Lat','Long','Province/State'], axis=1)
+
+
 df_conf_trans = df_conf.T
 
 #df_conf
